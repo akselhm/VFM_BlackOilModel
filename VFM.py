f171661a7ab76beaf5d246f88d1ae94e7ca39b0f
akselhm <akselhm@stud.ntnu.no>
2021-10-19 09:41:44 +0100

    progress on model

--- VFM.py
+++ VFM.py
@@ -1,33 +1,23 @@
 import numpy as np
-from blackoil import blackoil
+import matplotlib.pyplot as plt
+from blackoil import my_fluid
 
 
 
 # --- Variables ---
 
-P_atm = 100 #double check 
+# input data from tab 2 and 3 (do not need here??)
 
-
-# input data from tab 2 and 3
-
-API = 29.05         #API gravity (denoted gamma_API in tab)
-gamma_g0 = 0.83     #gas specific gravity at SC
-T_r = 333           #reservoir temperature
-P_b = 340*10**5     # bubble pressure at T_r
 GOR =196.4          #gas-oil ratio
-GORI =[]            #injection gas oil-ratio
 WOR =15.6           #water-oil ratio
 P_plat =22.2*10**5  # pressure at platform
 P_WCT =104.6*10**5  # pressure at WCT (used for testing)
-T_k =T_r            # temperature at WCT 
 j_o0 = 0.6860       # oil superficial velocity at SC
 eps = 0.00021       # roughness
 D = 0.1524          # pipe diameter
 
-
+g_s = 9.81      #gravity acceleration
 # ---
-
-gamma_g = gamma_g0 # from eq. 30
 
 theta = 0 #angle relative to the horizontal plane
 
@@ -40,3 +30,183 @@ maxit = 100
 #define under-relaxation factor
 urf = 0.1
 
+
+
+def massFlux_func(rho_l, rho_g, j_l, j_g):
+    #denoted G in the paper (eq 9)
+    return rho_l*j_l + rho_g*j_g 
+
+
+def void_frac_func(j, j_g, Cd, Ud):
+    #based on the drift-flux model (Zuber and Findlay, 1965) (eq 11)
+    return j_g/(Cd*j+Ud)
+
+def liquid_k_frac(j_k, j_sub, void_frac):
+    # function to calculate fraction of oil or water (eq 38 and 39)
+    # Ex: if calculating oil fraction, input should be (j_o, j_w,...)
+    return j_k/(j_k+j_sub)*(1-void_frac)
+
+
+#discretization
+
+def pressureStep(i, P_i, rho_m_avg, Z_avg):
+    # function for finding the pressure uptream P_(i-1) (eq 40)
+    # here we set Z = phi*f_l*G**2/(rho_l*D) for simplicity
+    return P_i + rho_m_avg*g_s*(z[i]-z[i-1]) + 0.5*Z_avg
+
+
+# --- equations from Appendix B (two-phase flow multiplier correlations) ---
+
+def DarcyFrictionFactor(Re):
+    # Darcy friction factor f
+    f = ((64/Re)**8 + 9.5*(np.log(eps/(3.7*D) + 5.74/(Re**0.9)) - (2500/Re)**6)**(-16))**0.125
+    return f
+
+def VieraGarcia(f, f_l, rho, rho_l):
+    #return: two-phase multiplier
+    # what friction factor f and density rho is inserted depends on what approach is used
+    return (f/f_l)*(rho_l/rho)
+
+
+
+def MullerHeck(f_l, f_g, rho_l, rho_g, G, x):
+    #return: two-phase multiplier
+    A = 0.5*f_l*G**2/(rho_l*D)
+    B = 0.5*f_g*G**2/(rho_g*D)
+    Gc = A+ 2*(B-A)*x
+    return (Gc*(1-x)**(1/3) + B*x**3)/A
+
+# --- equations from Appendix C (Void fraction correlations) ---
+
+
+def Cd_and_Ud_func(P, j, j_g, j_l, rho_g, rho_l, angle, method="B"): #might split to three funcs
+    # Drift flux correlations
+    # return: Cd, Ud
+    # ---------------------------------------
+    Cd, Ud = 0, 0
+    # default method Bendiksen(1984)
+    Fr_j = j/(np.sqrt(g_s*D))
+    if Fr_j < 3.5:
+        Cd = 1.05 + 0.15*np.sin(angle)
+        Ud = np.sqrt(g_s*D)*(0.35*np.sin(angle) + 0.54*np.cos(angle))
+    else: #Fr_j >= 3.5
+        Cd = 12
+        Ud = 0.35*np.sqrt(g_s*D)*np.sin(angle)
+    return Cd, Ud
+
+
+def run(blackoilmodel):
+    
+    P_list = np.zeros(N) # list for saving the pressure at each step
+    
+    #begin with the initial values at the platform
+    P_i = P_plat
+    
+    #initialize some values
+    rho_m_i = 0 
+    Z_i = 0
+    for i in range(N-1,-1,-1):  #find upstream pressure (iterate backwards)
+        #--define initial guess P_(i-1)^pred=P_i
+        P_pred=P_i
+        epsilon = 1000  #initialize epsilon to start iteration
+        it=0 # make sure the loop is not eternal
+        while epsilon>delta:
+        
+            # black-oil correlations, viscosities, and densities (make one function in myfluid-class to compute all)
+            R_so = blackoilmodel.R_so_func(P_pred)
+            B_o = blackoilmodel.B_o_func(P_pred, R_so)
+            B_w = blackoilmodel.B_w_func(P_pred)
+            Z_g = blackoilmodel.Z_g_func(P_pred)
+            R_sl = blackoilmodel.R_so_func(R_so)
+            B_l = blackoilmodel.B_l_func(B_w, B_o)
+            
+            rho_w = blackoilmodel.rho_w0/B_w    #function?
+            rho_g = blackoilmodel.rho_g_func(P_pred, Z_g)
+            rho_l = blackoilmodel.rho_l_func(R_sl, B_l)
+            rho_o = blackoilmodel.rho_o_func(R_so, B_o)
+            
+            visc_g = blackoilmodel.visc_g_func(rho_g)
+            visc_o = blackoilmodel.visc_o_func(P_pred, R_so)
+            visc_w = blackoilmodel.visc_w_func(P_pred)
+            
+            B_g = blackoilmodel.rho_g0/rho_g      # gas formation volume factor (function?)
+            
+            # find superficial velocities
+            j_g = B_g*j_o0*(GOR-R_so)
+            j_o = j_o0*B_o
+            j_w = j_o0*WOR*B_w
+            j_l = j_o + j_w
+            j   = j_l + j_g
+            
+            # void and liquid fractions
+            Cd, Ud = Cd_and_Ud_func(P_pred, j, j_g, j_l, rho_g, rho_l, theta, method='B') #Bendiksen (have not implemented the others)
+            void_frac = void_frac_func(j, j_g, Cd, Ud)
+            w_frac = liquid_k_frac(j_w, j_o, void_frac)
+            o_frac = liquid_k_frac(j_o, j_w, void_frac)
+            
+            # additional mixed properties (density and viscosity)
+            rho_m = rho_g*void_frac + rho_o*o_frac + rho_w*w_frac
+            visc_l = (visc_o*o_frac + visc_w*w_frac)/(o_frac + w_frac) #double check
+            visc_m = visc_g*void_frac + visc_o*o_frac + visc_w*w_frac
+            
+            G = rho_l*j_l + rho_g*j_g   #total mass flux
+            
+            # find friction factors        
+            Re_l = G*D/visc_l
+            f_l = DarcyFrictionFactor(Re_l)
+            
+            # find two phase multiplier (Vierra and Garcia or Muller and Heck)
+            # "VGm"(default), center of mass
+            v_m = (rho_g*j_g + rho_o*j_o + rho_w*j_w)/(rho_g*void_frac + rho_g*void_frac + rho_g*void_frac) #double check this eq
+            rho = rho_m
+            Re_cm = rho_m*v_m*D/visc_m
+            f = DarcyFrictionFactor(Re_cm)
+            phi = VieraGarcia(f, f_l, rho, rho_l)
+                    
+            
+            # compute averages
+            if i<N-1:
+                rho_m_avg = 0.5*(rho_m+rho_m_i) #ajust to 0.5*(rho_m+rho_m_pred) (not sure how to do it for the first step)
+            
+                Z=phi*f_l*G**2/(rho_l*D)
+                Z_avg = 0.5*(Z + Z_i) 
+                
+            else:   # initial step
+                rho_m_avg = rho_m #ajust to 0.5*(rho_m+rho_m_pred) (not sure how to do it for the first step)
+                
+                Z=phi*f_l*G**2/(rho_l*D)
+                Z_avg = Z #ajust 
+                
+            
+            #find pressure
+            P = pressureStep(i, P_i, rho_m_avg, Z_avg)
+             
+            # P iteration
+            # check convergence (compute epsilon)
+            epsilon = abs(P_pred-P)/P
+            
+            # under relaxed predicted value
+            #check covergence (epsilon)
+            P_pred += urf*(P-P_pred)
+            
+            #make sure the loop is not eternal
+            it+=it
+            if it>maxit:
+                print("max iterations reached. not convergent for P at step", i)
+                return
+        
+        #save variables for next step
+        P_list[i]=P
+        rho_m_i = rho_m
+        Z_i = Z
+        P_i = P
+    
+    return P_list
+
+fluid = my_fluid()
+
+pressures = run(my_fluid)
+
+fig = plt.figure()
+plt.plot(pressures)
+fig.savefig('results/pressureplot.png')
--- blackoil.py
+++ blackoil.py
@@ -1,71 +1,154 @@
-class blackoil:
+import numpy as np
+"""
+# input data from tab 2 and 3
 
-    def __init__(self, P):
-        P.self = P
+API = 29.05         #API gravity (denoted gamma_API in tab)
+gamma_g0 = 0.83     #gas specific gravity at SC
+T_r = 333           #reservoir temperature
+P_b = 340*10**5     # bubble pressure at T_r
+GOR =196.4          #gas-oil ratio
+GORI =[]            #injection gas oil-ratio
+WOR =15.6           #water-oil ratio
+P_plat =22.2*10**5  # pressure at platform
+P_WCT =104.6*10**5  # pressure at WCT (used for testing)
+T_k =T_r            # temperature at WCT 
+j_o0 = 0.6860       # oil superficial velocity at SC
+eps = 0.00021       # roughness
+D = 0.1524          # pipe diameter
 
-    def R_so_func(P):
-        #solution gas_oil ratio (eq A.1)
-        if P>P_b:
-            return GOR
-        #else: #P<=P_b
-            return gamma_g/C1*((C2*P/18.2 + 1.4) * 10**(0.0125*API-0.00091*C3) )**1.2048
+gamma_g = gamma_g0 # from eq. 30
+
+"""
+class my_fluid:
+    #the black oil model. Initial imput values from tab 2 and 3
+    def __init__(self, API = 29.05, gamma_g0 = 0.83, T_r = 333, P_b = 340*10**5, 
+    GOR =196.4, WOR =15.6, P_plat =22.2*10**5, P_WCT =104.6*10**5, j_o0 = 0.6860):
         
-    def B_o_func(P, R_so):
+        self.API = API         #API gravity (denoted gamma_API in tab)
+        self.gamma_g0 = 0.83     #gas specific gravity at SC
+        self.T_r = 333           #reservoir temperature
+        self.P_b = 340*10**5     # bubble pressure at T_r
+        self.GOR =196.4          #gas-oil ratio
+        self.WOR =15.6           #water-oil ratio
+        self.P_plat =22.2*10**5  # pressure at platform (do not need)
+        self.P_WCT =104.6*10**5  # pressure at WCT (used for testing) (migth not need here?)
+        self.T_k =T_r            # temperature at WCT 
+        self.j_o0 = 0.6860       # oil superficial velocity at SC
+
+        self.gamma_g = gamma_g0 # from eq. 30
+
+        # -- from Appendix A (black oil correlations) ---
+        # input from tab A.5 (black oil model)
+
+        self.C_gas = 8.314   # universial gas constant (denoted /\ )
+        self.M_a = 0.028966  #Air molar mass
+        self.rho_g0 = 1.223202 #air/gas density at standard conditions, denoted rho_g0 in table
+        self.rho_w0 = 999.14 # water density at standard conditions 
+
+        self.C1 = 5.61456349 
+        self.C2 = 0.000145038
+        self.C3 = 1.8*self.T_k-459.67
+        self.C4 = 0.001
+        self.C5 = 1.8
+
+        # --- define some more values at SC ---
+
+        self.gamma_o0 = 141.5/(API+131.5)    #specific gravity of oil at SC (eq 22)
+
+        self.rho_o0 = self.gamma_o0*self.rho_w0        #density of oil at SC (eq 23)
+
+        self.rho_l0 = (WOR/(1+WOR))*self.rho_w0 + (1/(1+WOR))*self.rho_o0 #liquid density at SC (eq 26)
+
+        self.P_atm = 100325       #NEED GLOBAL VARIABLE?
+
+    def R_so_func(self, P):
+        #solution gas_oil ratio (eq A.1)
+        if P>self.P_b:
+            return self.GOR
+        #else: #P<=P_b
+        return self.gamma_g/self.C1*((self.C2*P/18.2 + 1.4) * 10**(0.0125*self.API-0.00091*self.C3) )**1.2048
+        
+    def B_o_func(self, P, R_so):
         #oil formation volume factor (eq A.2+)
-        B_o = 0.9759 + 0.00012*(C1*R_so*(gamma_g0/gamma_o0)**0.5 + 1.25*C3)**1.2
-        if P <= P_b:
+        B_o = 0.9759 + 0.00012*(self.C1*R_so*(self.gamma_g0/self.gamma_o0)**0.5 + 1.25*self.C3)**1.2
+        if P <= self.P_b:
             return B_o
         else:
-            c0 = (-1433 + 5*C1*GOR + 17.2*C3 - 1180*gamma_g0 + 12.61*API)/(P*10**5)
-            return B_o*np.exp(-c0*(P-P_b))
+            c0 = (-1433 + 5*self.C1*self.GOR + 17.2*self.C3 - 1180*self.gamma_g0 + 12.61*self.API)/(P*10**5)
+            return B_o*np.exp(-c0*(P-self.P_b))
 
-    def B_w_func(P):
+    def B_w_func(self, P):
         # water formation volume factor (eq A.5+)
-        k1 = 5.50654*10**(-7) *(C3)**2
-        k2 = -1.72834*10**(-13) *C2**2 *C3*P**2
-        k3 = -3.58922*10**(-7) *C2*P
-        k4 = -2.25341*10**(-10) *C2**2 *P**2
-        dV_wT = -1.0001*10**(-2) + 1.33391*10**(-4)*C3 + k1
-        dV_wP = -1.95301*10**(-9)*C2*C3*P + k2 + k3 + k4
+        k1 = 5.50654*10**(-7) *(self.C3)**2
+        k2 = -1.72834*10**(-13) *self.C2**2 *self.C3*P**2
+        k3 = -3.58922*10**(-7) *self.C2*P
+        k4 = -2.25341*10**(-10) *self.C2**2 *P**2
+        dV_wT = -1.0001*10**(-2) + 1.33391*10**(-4)*self.C3 + k1
+        dV_wP = -1.95301*10**(-9)*self.C2*self.C3*P + k2 + k3 + k4
         return (1+dV_wP)*(1+dV_wT)
 
-    def Z_g_func(P):
+    def Z_g_func(self, P):
         #gas compressibility factor    (eq A.12+)
         # calculate pseudo critical pressure and temperature
-        P_pc = (677 + 15.0*gamma_g - 37.5*gamma_g**2)/C2
-        T_pc = (168 + 325*gamma_g - 12.5*gamma_g**2)/C5
+        P_pc = (677 + 15.0*self.gamma_g - 37.5*self.gamma_g**2)/self.C2
+        T_pc = (168 + 325*self.gamma_g - 12.5*self.gamma_g**2)/self.C5
         
         P_pr=P/P_pc
-        T_pr=T_k/T_pc
+        T_pr=self.T_k/T_pc
         return 1 - (3.52*P_pr/(10**(0.9813*T_pr))) + (0.274*P_pr**2/(10**(0.8157*T_pr)))
 
-    def visc_o_func(P, R_so):
+    def visc_o_func(self, P, R_so):
         #oil viscosity (eq A.17+)
-        m1 = 10**(0.43 + 8.33/API)
-        u_od = C4*(0.32 + (1.8*10**7)/(API**4.53))*(360/(C3+200))**m1 
+        m1 = 10**(0.43 + 8.33/self.API)
+        u_od = self.C4*(0.32 + (1.8*10**7)/(self.API**4.53))*(360/(self.C3+200))**m1 
         if P <= P_atm:    #P==P_atm in paper
             return u_od 
         else:
-            k5 = (u_od/C4)**(5.44*(C1*R_so+150)**(-0.338))
-            u_o = 10.715*C4*(C1*R_so + 100)**(-0.515)*k5
+            k5 = (u_od/self.C4)**(5.44*(self.C1*R_so+150)**(-0.338))
+            u_o = 10.715*self.C4*(self.C1*R_so + 100)**(-0.515)*k5
             if P <= P_b:
                 return u_o
             #else
-            k6 = -11.513 - 8.98*10**(-5)*C2*P
-            m2 = 2.6*(C2*P)**1.187*np.exp(k6)
-            return u_o*(P/P_b)**m2
+            k6 = -11.513 - 8.98*10**(-5)*self.C2*P
+            m2 = 2.6*(self.C2*P)**1.187*np.exp(k6)
+            return u_o*(P/self.P_b)**m2
         
-    def visc_g_func(rho_g):
+    def visc_g_func(self, rho_g):
         #gas viscosity
-        M_g = gamma_g*M_a #gas molar mass
-        F1 = ((9.379+16.07*M_g)*(C5*T_k)**1.5)/(209.2+19260*M_g+C5*T_k)
-        F2 = 3.448 + 986.4/(C5*T_k) + 10.09*M_g
+        M_g = self.gamma_g*self.M_a #gas molar mass
+        F1 = ((9.379+16.07*M_g)*(self.C5*self.T_k)**1.5)/(209.2+19260*M_g+self.C5*self.T_k)
+        F2 = 3.448 + 986.4/(self.C5*self.T_k) + 10.09*M_g
         F3 = 2.447 - 0.2224*F2
-        return C4*F1*10**(-4)*np.exp(F2*(C4*rho_g)**F3)
+        return self.C4*F1*10**(-4)*np.exp(F2*(self.C4*rho_g)**F3)
 
-    def visc_w_func(P):
+    def visc_w_func(self, P):
         #water wiscosity
-        u_w0 = 109.574*C4*C3**(-1.12166)
-        k7 = C2*P + 14.7
+        u_w0 = 109.574*self.C4*self.C3**(-1.12166)
+        k7 = self.C2*P + 14.7
         return u_w0*(0.9994 + 4.0295*10**(-5)*k7 + 3.1062*10**(-9)*k7**2)
+
+
+    def R_sl_func(self, R_so):
+        #solution gas-liquid(?) ratio (eq 17)
+        return (1/(1+WOR))*R_so
+
+    def B_l_func(self, B_w, B_o):
+        # liquid formation volume factor (eq 21)
+        return (WOR/(1+WOR))*B_w + (1/(1+WOR))*B_o
+
+    def B_g_func(self, rho_g):
+        return rho_g0/rho_g
+
+    def rho_g_func(self, P, Z_g):
+        # gas density   (eq 28)
+        return self.gamma_g*self.M_a*P/(self.C_gas*self.T_r*Z_g)
+
+    def rho_o_func(self, R_so, B_o):
+        # density oil  (eq 24)
+        return (self.rho_o0 + self.rho_g0*R_so)/B_o
+
+    def rho_l_func(self, R_sl, B_l):
+        # density liquid (eq 25)
+        return (self.rho_g0*R_sl+self.rho_l0)/B_l 
+
 
